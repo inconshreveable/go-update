4fe99b0604155a266bfcf549542fdf8ece0c2b9b
Cedric BAIL <cedric@ddlm.me>
2022-06-01 14:10:41 -0600

    Add low level API to Restart.
    Currently following other public API, but we might later on drop it to
    internal only.

---
+++ restart.go
@@ -0,0 +1,32 @@
+package selfupdate
+
+import (
+	"os"
+	"syscall"
+
+	"github.com/fynelabs/selfupdate/internal/osext"
+)
+
+func Restart() error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	exe, err := osext.Executable()
+	if err != nil {
+		return err
+	}
+
+	p, err := os.StartProcess(exe, os.Args, &os.ProcAttr{
+		Dir:   wd,
+		Env:   os.Environ(),
+		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+		Sys:   &syscall.SysProcAttr{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
