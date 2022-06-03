c563048db5eb45666ff25d05e743549c58f90f44
Cedric BAIL <cedric@ddlm.me>
2022-06-03 14:37:32 -0600

    We should actually handle EOF by calling the progress callback with 1 and no error.

--- progress.go
+++ progress.go
@@ -17,10 +17,14 @@ func (pr *progressReader) Read(p []byte) (int, error) {
 	n, err := pr.Reader.Read(p)
 	pr.downloaded += int64(n)
 
-	if pr.contentLength > 0 {
-		pr.progressCallback(float64(pr.downloaded)/float64(pr.contentLength), err)
+	if err != io.EOF {
+		if pr.contentLength > 0 {
+			pr.progressCallback(float64(pr.downloaded)/float64(pr.contentLength), err)
+		} else {
+			pr.progressCallback(float64(pr.contentLength), err)
+		}
 	} else {
-		pr.progressCallback(float64(pr.contentLength), err)
+		pr.progressCallback(1, nil)
 	}
 
 	return n, err
