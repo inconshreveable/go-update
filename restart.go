bd28f6d9caeedf96af7bbbc82c64d33c6e4cefbc
Cedric BAIL <cedric@ddlm.me>
2022-06-06 15:50:10 -0600

    Add documentation for new Restart parameter.

--- restart.go
+++ restart.go
@@ -10,6 +10,8 @@ import (
 // Restart will attempt to restar the current application, any error will be returned.
 // If the exiter function is passed in it will be responsible for terminating the old processes.
 // If exiter is passed an error it can assume the restart failed and handle appropriately.
+// It is recommended to provide the executable that need to be started, but if none is
+// specified we will try to guess it correctly.
 func Restart(exiter func(error), executable string) error {
 	wd, err := os.Getwd()
 	if err != nil {
