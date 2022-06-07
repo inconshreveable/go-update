90a4306b1d3de4d44f30d0429dd333eadcec6eec
Cedric BAIL <cedric@ddlm.me>
2022-06-07 13:55:00 -0600

    Update command comments accordingly.

--- cmd/selfupdatectl/upload.go
+++ cmd/selfupdatectl/upload.go
@@ -23,8 +23,8 @@ func s3upload() *cli.Command {
 
 	return &cli.Command{
 		Name:        "s3upload",
-		Usage:       "Upload multiple executable which has been properly signed to a S3 path",
-		Description: "The executable specified will get their signature generated and checked before being uploaded to a S3 bucket location specified as the last arguments.",
+		Usage:       "Upload an executable file to S3, it will be signed and the signature uploaded too",
+		Description: "The executable specified will get his signature generated and checked before being uploaded to a S3 bucket location specified as the last arguments.",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "private-key",
