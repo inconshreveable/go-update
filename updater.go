62b4c299e37ec024629ffe4a6200b9ab9081308e
Cedric BAIL <cedric@ddlm.me>
2022-06-13 17:54:18 -0600

    Reduce complexity and only use one nested go func.

--- updater.go
+++ updater.go
@@ -33,7 +33,6 @@ type Repeating int
 
 const (
 	None    Repeating = iota
-	Minutly           // Will schedule in the next minute and repeat it every minute after
 	Hourly            // Will schedule in the next hour and repeat it every hour after
 	Daily             // Will schedule next day and repeat it every day after
 	Monthly           // Will schedule next month and repeat it every month after
@@ -136,37 +135,9 @@ func Manage(conf *Config) (*Updater, error) {
 			}
 		}
 
-		if updater.conf.Schedule.Interval != 0 {
+		if updater.conf.Schedule.Interval != 0 || updater.conf.Schedule.At.Repeating != None {
 			go func() {
-				for {
-					time.Sleep(updater.conf.Schedule.Interval)
-					logInfo("Scheduled upgrade check after %s.\n", updater.conf.Schedule.Interval)
-					err := updater.CheckNow()
-					if err != nil {
-						logError("Upgrade error: %v\n", err)
-					}
-				}
-			}()
-		}
-
-		if updater.conf.Schedule.At.Repeating != None {
-			go func() {
-				for {
-					now := time.Now()
-					var next time.Time
-					switch updater.conf.Schedule.At.Repeating {
-					case Minutly:
-						next = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute()+1, updater.conf.Schedule.At.Second(), updater.conf.Schedule.At.Nanosecond(), time.Local)
-					case Hourly:
-						next = time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, updater.conf.Schedule.At.Minute(), updater.conf.Schedule.At.Second(), updater.conf.Schedule.At.Nanosecond(), time.Local)
-					case Daily:
-						next = time.Date(now.Year(), now.Month(), now.Day()+1, updater.conf.Schedule.At.Hour(), updater.conf.Schedule.At.Minute(), updater.conf.Schedule.At.Second(), updater.conf.Schedule.At.Nanosecond(), time.Local)
-					case Monthly:
-						next = time.Date(now.Year(), now.Month()+1, updater.conf.Schedule.At.Day(), updater.conf.Schedule.At.Hour(), updater.conf.Schedule.At.Minute(), updater.conf.Schedule.At.Second(), updater.conf.Schedule.At.Nanosecond(), time.Local)
-					}
-					time.Sleep(next.Sub(now))
-					updater.CheckNow()
-				}
+				triggerSchedule(updater)
 			}()
 		}
 	}()
@@ -202,4 +173,42 @@ func applyUpdate(r io.Reader, publicKey ed25519.PublicKey, signature [64]byte) (
 		return "", err
 	}
 	return opts.TargetPath, nil
+}
+
+func triggerSchedule(updater *Updater) {
+	for {
+		var next time.Duration
+
+		if updater.conf.Schedule.Interval != 0 {
+			next = updater.conf.Schedule.Interval
+		}
+		if updater.conf.Schedule.At.Repeating != None {
+			at := nextScheduleAt(updater.conf.Schedule.At.Repeating, updater.conf.Schedule.At.Time)
+			if next == 0 || at < next {
+				next = at
+			}
+		}
+
+		time.Sleep(next)
+		logInfo("Scheduled upgrade check after %s.\n", next)
+		err := updater.CheckNow()
+		if err != nil {
+			logError("Upgrade error: %v\n", err)
+		}
+	}
+}
+
+func nextScheduleAt(repeating Repeating, offset time.Time) time.Duration {
+	now := time.Now()
+	var next time.Time
+	switch repeating {
+	case Hourly:
+		next = time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, offset.Minute(), offset.Second(), offset.Nanosecond(), time.Local)
+	case Daily:
+		next = time.Date(now.Year(), now.Month(), now.Day()+1, offset.Hour(), offset.Minute(), offset.Second(), offset.Nanosecond(), time.Local)
+	case Monthly:
+		next = time.Date(now.Year(), now.Month()+1, offset.Day(), offset.Hour(), offset.Minute(), offset.Second(), offset.Nanosecond(), time.Local)
+	}
+
+	return next.Sub(now)
 }
